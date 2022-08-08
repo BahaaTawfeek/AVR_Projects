612f4f28aa707073a2d0485ed2ea62ddb3ea4346
BahaaTawfeek <107799988+BahaaTawfeek@users.noreply.github.com>
2022-08-09 01:20:09 +0200

    Add files via upload

---
+++ Distance Measuring System/Code/Distance Measuring System/Distance Measuring System/DIO.h
@@ -0,0 +1,33 @@
+﻿/*
+ * DIO.h
+ *
+ * Created: 26/04/2022 01:22:04 am
+ *  Author: Bahaa Tawfik
+ */
+ 
+
+
+#ifndef DIO_H_
+#define DIO_H_
+
+void DIO_vsetPinDir(unsigned char portname,unsigned char pinnumber,unsigned char direction);
+void DIO_vsetPortDir(unsigned char portname,unsigned char direction);
+
+void DIO_vwritePin(unsigned char portname,unsigned char pinnumber,unsigned char value);
+void DIO_vwritePort(unsigned char portname,unsigned char value);
+
+unsigned char DIO_u8readPin(unsigned char portname,unsigned char pinnumber);
+unsigned char DIO_u8readPort(unsigned char portname);
+
+void DIO_vtogglePin(unsigned char portname,unsigned char pinnumber);
+//void DIO_vtogglePort(unsigned char portname);
+
+
+void DIO_vconnectPullup(char portname ,char pinnumber, char connect_pullup);
+
+void DIO_vwrite_low_nibble(unsigned char portname,unsigned char value);
+void DIO_vwrite_high_nibble(unsigned char portname,unsigned char value);
+
+
+
+#endif /* DIO_H_ */
---
+++ Distance Measuring System/Code/Distance Measuring System/Distance Measuring System/Distance Measuring System.c
@@ -0,0 +1,73 @@
+﻿/*
+ * Distance_Measuring_System.c
+ 
+ 
+ 
+ *
+ * Created: 27/05/2022 02:51:21 
+ *  Author: Bahaa Tawfik    / / 
+ */ 
+
+#include <avr/io.h>
+#include "LCD.h"
+#define  F_CPU 1000000UL
+#include <util/delay.h>
+int main(void)
+{
+	
+	LCD_vInit();
+	unsigned short a,b,high,distance;
+	
+	DIO_vsetPinDir('D',7,1);			// make a direction of trigger pin of Ultrasonic Sensor is output pin
+	DIO_vsetPinDir('D',6,0);			// make a direction of echo pin of Ultrasonic Sensor is output pin
+	
+
+	while(1)
+	{
+		TCCR1A=0;		/* Set all bit to zero Normal operation */
+		TIFR = 1<<ICF1;				/* Clear ICP flag (Input Capture flag) */
+        /* Give  more than 10us trigger pulse on trig. pin to US */
+        DIO_vwritePin('D',7,1);
+        _delay_us(50);
+        DIO_vwritePin('D',7,0);
+        _delay_us(50);			// not need 
+		
+		TCCR1B = 0xc1;				/* Capture on rising edge, No prescaler, noise cancel*/ 
+		while ((TIFR&(1<<ICF1)) == 0);		//mentor ICF falg until set ===== note () is important for periorty 
+		a=ICR1;					  // Take value of capute register
+		TIFR=(1<<ICF1);			 // clear ICF1 flag
+		
+		TCCR1B= 0x81;		/* Capture on failing edge, No prescaler, noise cancel*/
+		while ((TIFR&(1<<ICF1)) == 0);		//mentor ICF falg until set ===== note () is important for periorty 
+		b=ICR1;					  // Take value of capute register
+		TIFR=(1<<ICF1);			 // clear ICF1 flag
+		TCNT1=0;		// Clear Timer counter 
+		TCCR1B=0;		// stop timer
+
+/*Calculation (distance in cm) 
+ *Sound velocity =   343.00 m/s = 34300 cm/s
+ */	
+		high=b-a;
+		distance = ((high*34600)/(F_CPU*2)) ;     
+		
+		if (distance<=80)
+		{
+			LCD_vclearscreen();
+			LCD_vsend_string("distance=");
+			LCD_vsend_char((distance/10)+48);
+			LCD_vsend_char((distance%10)+48);
+			LCD_vsend_string("cm");
+			_delay_ms(500);	
+		}
+		else
+		{
+			LCD_vclearscreen();
+			LCD_vsend_string("No Object Found,");
+			LCD_vmovecursor(2,0);
+			LCD_vsend_string("MaxDetect.. 80cm"); 
+			_delay_ms(500);		
+		}
+	}//end of while
+}//end of main function
+
+
